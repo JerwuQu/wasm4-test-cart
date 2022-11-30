ceecebdd46e1c84f3196ccb67262a24a6ce6d85b
Marcus Ramse <marcus@ramse.se>
2022-11-30 23:39:30 +0100

    Log test results

--- src/main.zig
+++ src/main.zig
@@ -1,39 +1,50 @@
 const std = @import("std");
 const w4 = @import("wasm4.zig");
 
-fn assertEqual(a: anytype, b: @TypeOf(a)) void {
-    if (a != b) {
-        //var buf: [512]u8 = undefined;
-        //w4.trace(std.fmt.bufPrint(&buf, "Oh no... 0x{x} 0x{x}", .{a, b}) catch unreachable);
-        w4.trace("Failed");
+var testState = struct {
+    passed: usize = 0,
+    failed: usize = 0,
+}{};
+
+fn assertFailed(comptime src: std.builtin.SourceLocation) void {
+    w4.tracef("!!! Failed: %s (line %d)", src.fn_name.ptr, src.line);
+    testState.failed += 1;
+}
+
+fn assert(comptime src: std.builtin.SourceLocation, condition: bool) void {
+    if (condition) {
+        testState.passed += 1;
+    } else {
+        assertFailed(src);
     }
 }
 
-fn assertFilledWith(buf: []const u8, value: u8) void {
+fn assertFilled(comptime src: std.builtin.SourceLocation, buf: []const u8, value: u8) void {
     for (buf) |c| {
         if (c != value) {
-            w4.trace("Failed");
+            assertFailed(src);
             return;
         }
     }
+    testState.passed += 1;
 }
 
 // Test initial memory state
 fn test_initial_memory() void {
     // PALETTE
-    assertEqual(w4.PALETTE[0], 0xe0f8cf);
-    assertEqual(w4.PALETTE[1], 0x86c06c);
-    assertEqual(w4.PALETTE[2], 0x306850);
-    assertEqual(w4.PALETTE[3], 0x071821);
+    assert(@src(), w4.PALETTE[0] == 0xe0f8cf);
+    assert(@src(), w4.PALETTE[1] == 0x86c06c);
+    assert(@src(), w4.PALETTE[2] == 0x306850);
+    assert(@src(), w4.PALETTE[3] == 0x071821);
 
     // DRAW_COLORS
-    assertEqual(w4.DRAW_COLORS.*, 0x1203);
+    assert(@src(), w4.DRAW_COLORS.* == 0x1203);
 
     // SYSTEM_FLAGS
-    assertEqual(w4.SYSTEM_FLAGS.*, 0);
+    assert(@src(), w4.SYSTEM_FLAGS.* == 0);
 
     // FRAMEBUFFER
-    assertFilledWith(w4.FRAMEBUFFER, 0);
+    assertFilled(@src(), w4.FRAMEBUFFER, 0);
 }
 
 // Test disk capabilities
@@ -50,31 +61,31 @@ fn test_disk() void {
     // Single byte (sz = 1)
     std.mem.set(u8, &bufIn, BYTE_UNUSED_IN);
     bufIn[0] = BYTE_COPIED;
-    assertEqual(w4.diskw(&bufIn, 1), 1);
+    assert(@src(), w4.diskw(&bufIn, 1) == 1);
 
     std.mem.set(u8, &bufOut, BYTE_UNUSED_OUT);
-    assertEqual(w4.diskr(&bufOut, 1), 1);
-    assertEqual(bufOut[0], BYTE_COPIED);
-    assertFilledWith(bufOut[1..], BYTE_UNUSED_OUT);
+    assert(@src(), w4.diskr(&bufOut, 1) == 1);
+    assert(@src(), bufOut[0] == BYTE_COPIED);
+    assertFilled(@src(), bufOut[1..], BYTE_UNUSED_OUT);
 
     // Max size disk (sz = 1024)
     std.mem.set(u8, bufIn[0..DISK_MAX], BYTE_COPIED);
     std.mem.set(u8, bufIn[DISK_MAX..], BYTE_UNUSED_IN);
-    assertEqual(w4.diskw(&bufIn, DISK_MAX), DISK_MAX);
+    assert(@src(), w4.diskw(&bufIn, DISK_MAX) == DISK_MAX);
 
     std.mem.set(u8, &bufOut, BYTE_UNUSED_OUT);
-    assertEqual(w4.diskr(&bufOut, DISK_MAX), DISK_MAX);
-    assertFilledWith(bufOut[0..DISK_MAX], BYTE_COPIED);
-    assertFilledWith(bufOut[DISK_MAX..], BYTE_UNUSED_OUT);
+    assert(@src(), w4.diskr(&bufOut, DISK_MAX) == DISK_MAX);
+    assertFilled(@src(), bufOut[0..DISK_MAX], BYTE_COPIED);
+    assertFilled(@src(), bufOut[DISK_MAX..], BYTE_UNUSED_OUT);
 
     // Overflowing disk (sz = 2048)
     std.mem.set(u8, &bufIn, BYTE_COPIED);
-    assertEqual(w4.diskw(&bufIn, DISK_MAX_2), DISK_MAX);
+    assert(@src(), w4.diskw(&bufIn, DISK_MAX_2) == DISK_MAX);
 
     std.mem.set(u8, &bufOut, BYTE_UNUSED_OUT);
-    assertEqual(w4.diskr(&bufOut, DISK_MAX_2), DISK_MAX);
-    assertFilledWith(bufOut[0..DISK_MAX], BYTE_COPIED);
-    assertFilledWith(bufOut[DISK_MAX..], BYTE_UNUSED_OUT);
+    assert(@src(), w4.diskr(&bufOut, DISK_MAX_2) == DISK_MAX);
+    assertFilled(@src(), bufOut[0..DISK_MAX], BYTE_COPIED);
+    assertFilled(@src(), bufOut[DISK_MAX..], BYTE_UNUSED_OUT);
 }
 
 // Test persistency through `update`
@@ -91,15 +102,24 @@ fn test_draw_operations() void {
     // - blitSub
     // - text
     // etc...
+
+    // TODO: allow for inspecting failed tests visually
 }
 
 export fn start() void {
-    w4.trace("gfx-test-cart started");
+    w4.trace("Test suite started");
     test_initial_memory();
     test_disk();
     test_update_persistance();
     test_draw_operations();
-    w4.trace("gfx-test-cart finished");
+
+    w4.tracef("Passed: %d", testState.passed);
+    w4.tracef("Failed: %d", testState.failed);
+    if (testState.failed == 0) {
+        w4.PALETTE[0] = 0x88ff88;
+    } else {
+        w4.PALETTE[0] = 0xff8888;
+    }
 }
 
 export fn update() void {}
